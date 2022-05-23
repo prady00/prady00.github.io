31403cafd877b045903e4e1700540f0b8808bdc8
Pradeep Kumar <pradeep.online00@gmail.com>
2022-05-23 08:48:57 +0000

    added about section

---
+++ env.bat
@@ -0,0 +1,21 @@
+set PROJECT_HOME="C:\Projects\prady00.github.io\source"
+
+set JAVA_HOME=C:\Apps\jdk1.8.0_131
+set M2_HOME=C:\Emirates\Java_tools\apache-maven-3.5.4
+
+set NOD_HOME=C:\Emirates\Node_tools\node-v8.11.3-win-x64
+set NVM_HOME=C:\Emirates\Node_tools\node-v8.11.3-win-x64
+set NVM_SYMLINK=C:\Emirates\Node_tools\nvm\root\nodejs
+set RUBY_HOME=C:\Apps\Ruby23\bin
+set PATH=%NOD_HOME%;%RUBY_HOME%;%NVM_HOME%;%NVM_HOME%\tools\python;%NVM_SYMLINK%;%JAVA_HOME%\bin;%M2_HOME%\bin;%PATH%;
+
+set HTTP_PROXY=http://S391348:May2018@zscaler.emirates.com:10068
+set HTTPS_PROXY=http://S391348:May2018@zscaler.emirates.com:10068
+set NO_PROXY=.emirates.com
+
+
+c:
+
+cd %PROJECT_HOME%
+
+cmd
